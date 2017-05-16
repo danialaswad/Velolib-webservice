9e356f270d81c894aaac013f5d5dd1dfc633c242
Danial Aswad Bin Ahmad Fazlan <danial-aswad.bin-ahmad-fazlan@etu.unice.fr>
2017-05-17 01:29:58 +0200

    Petit modif

--- ClientWAF/Form1.Designer.cs
+++ ClientWAF/Form1.Designer.cs
@@ -68,6 +68,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(112, 93);
             this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
             this.richTextBox1.Size = new System.Drawing.Size(581, 220);
             this.richTextBox1.TabIndex = 3;
             this.richTextBox1.Text = "";
